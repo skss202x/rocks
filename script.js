4360cceb8301a564493c2d033603b34aec221c1a
skss202x <skss202x@outlook.com>
2025-05-26 09:51:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-// Total number of rock images you have (update this if you add more)
-const totalImages = 478; // Example: you have rock1.jpg to rock10.jpg
-
-// Folder where images are stored
+const totalImages = 10; // Change this to however many images you have
 const imagePath = "pictures/";
-
-// Reference to the <img> tag
 const imageElement = document.getElementById("randomImage");
 
-// Function to show a random rock image
 function showRandomImage() {
-  const randomIndex = Math.floor(Math.random() * totalImages) + 1; // Generates 1 to totalImages
-  const filename = `rock${randomIndex}.jpg`;
+  const randomIndex = Math.floor(Math.random() * totalImages) + 1;
+  const filename = `${randomIndex}.jpg`;
   imageElement.src = imagePath + filename;
 }
 
-// Load a random image when the page first loads
 window.onload = showRandomImage;
